ef22fcf2fc6ed12b99d739d2b09cd7467c8800eb
Gengyuan Huang <gengyuan@ualberta.ca>
2022-05-01 23:32:04 -0600

    Install tencent cloud sdk and add emotion analysis model

---
+++ backend/WNFA_django_RESTful/WNFA_text_to_art_generator/emotion_analysis.py
@@ -0,0 +1,10 @@
+import joblib 
+pipe = joblib.load(open("models/emotion_classifier_pipe.pkl","rb"))
+
+def predict_emo(text):
+	classes = pipe.classes_
+	results = pipe.predict_proba([text])
+	results_dict = dict()
+	for index in range(len(classes)):
+		results_dict[classes[index]] = results[0][index]
+	return results_dict

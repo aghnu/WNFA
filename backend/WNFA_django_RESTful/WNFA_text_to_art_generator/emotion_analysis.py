7b2e99dd3b3620a8eabf45f0f8f7ac41a9c90352
Gengyuan Huang <gengyuan@ualberta.ca>
2022-05-13 20:08:58 -0600

    Remove the randomness of binary text generator

--- backend/WNFA_django_RESTful/WNFA_text_to_art_generator/emotion_analysis.py
+++ backend/WNFA_django_RESTful/WNFA_text_to_art_generator/emotion_analysis.py
@@ -13,14 +13,7 @@ def predict_emo(text):
 	return results_dict
 
 testing = '''
-There once was a spirit named Yuzhen,
-Who went oft to the peak of Taihua.                                        
-At dawn she would strike a heavenly drum,                                                     
-Soaring and prancing on twin dragons.
-
-Struck by lightening, she still didn't stop,
-Traveling through clouds, she left not a trace.                          
-When she come to Mt. Shaoshi,
-Who shall she meet but Wangmu (Queen Mother).
+        Silent spring is to cherish the fine flow of water, the shade of trees according to the water is soft and sunny.
+        The little tender lotus had just revealed the tightly wrapped tip of the leaf, and the lovely dragonfly stood on it.
 '''
 
